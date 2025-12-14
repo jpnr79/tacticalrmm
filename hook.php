b76a1561c7463b9959b0abb010f4b6464fe30cc7
Pedro Rocha <jpnr79@gmail.com>
2025-12-14 18:14:09 +0000

    Refactor installation logic in TacticalRMM plugin; change `id` field to unsigned integer

--- hook.php
+++ hook.php
@@ -21,7 +21,7 @@ function plugin_tacticalrmm_install(): bool
     global $DB;
     $table = "glpi_plugin_tacticalrmm_configs";
     $query = "CREATE TABLE IF NOT EXISTS `$table` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
         `url` varchar(255) NOT NULL DEFAULT '',
         `field` varchar(255) NOT NULL DEFAULT 'serial',
         PRIMARY KEY (`id`)
