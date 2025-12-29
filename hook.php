e84ebd49a6b5beeb2ca367ec40b55191f4403d26
jpnr79 <jpnr79@gmail.com>
2025-12-29 02:45:45 +0000

    Refactor installation logic in TacticalRMM plugin; replace deprecated `doQuery` method with `request` for database operations

--- hook.php
+++ hook.php
@@ -31,29 +31,34 @@ if (!defined('GLPI_ROOT')) { define('GLPI_ROOT', realpath(__DIR__ . '/../..')); 
             `field` varchar(255) NOT NULL DEFAULT 'serial',
             PRIMARY KEY (`id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
-        $DB->doQuery($query);
+        $DB->request($query);
     }
 
     // Ensure id is unsigned if table already exists (check column type first)
     $need_alter = false;
+    $dbname = '';
+    $res = $DB->request("SELECT DATABASE() AS dbname");
+    if ($res && count($res) && isset($res[0]['dbname'])) {
+        $dbname = $res[0]['dbname'];
+    }
     $col = $DB->request(["FROM" => "information_schema.COLUMNS", "WHERE" => [
-        "TABLE_SCHEMA" => $DB->getDBName(),
+        "TABLE_SCHEMA" => $dbname,
         "TABLE_NAME" => $table,
         "COLUMN_NAME" => 'id'
     ]]);
-    if ($col && $row = $col->next()) {
-        if (stripos($row['COLUMN_TYPE'], 'unsigned') === false) {
+    if ($col && count($col) && isset($col[0]['COLUMN_TYPE'])) {
+        if (stripos($col[0]['COLUMN_TYPE'], 'unsigned') === false) {
             $need_alter = true;
         }
     }
     if ($need_alter) {
         $alter = "ALTER TABLE `$table` MODIFY COLUMN `id` int(11) unsigned NOT NULL AUTO_INCREMENT;";
-        $DB->doQuery($alter);
+        $DB->request($alter);
     }
 
     // Insert default row if missing
     $insert = "INSERT IGNORE INTO `$table` (`id`, `url`, `field`) VALUES (1, '', 'serial');";
-    $DB->doQuery($insert);
+    $DB->request($insert);
     return true;
 }
 
@@ -68,7 +73,7 @@ function plugin_tacticalrmm_uninstall(): bool
 {
     global $DB;
     $table = "glpi_plugin_tacticalrmm_configs";
-    $DB->doQuery("DROP TABLE IF EXISTS `$table`;");
+    $DB->request("DROP TABLE IF EXISTS `$table`;");
     return true;
 }
 
